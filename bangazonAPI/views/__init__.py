c0176d70e3200fa77ca9653a7c9cd0f6c406ad29
Cherkesky <guy@cherkesky.com>
2020-02-24 11:07:28 -0600

    initial get one get all viewsets and init importing

---
+++ bangazonAPI/views/__init__.py
@@ -0,0 +1,1 @@
+from .v_product import Product
---
+++ bangazonAPI/views/v_product.py
@@ -0,0 +1,52 @@
+"""View module for handling requests about products"""
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from bangazonAPI.models import Product
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    """JSON serializer for products
+
+    Arguments:
+        serializers.HyperlinkedModelSerializer
+    """
+    class Meta:
+        model = Product
+        url = serializers.HyperlinkedIdentityField(
+            view_name='product',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'name', 'customer', 'price', 'description','quantity', 'location', 'image_path', 'created_at', 'product_type')
+
+class Products (ViewSet):
+    """Products for Bangazon"""
+    # handles GET all
+    def list(self, request):
+        """Handle GET requests for all products
+
+        Returns:
+            Response -- JSON serialized product instance
+        """
+        products = Product.objects.all()
+        serializer = ProductSerializer(
+            products,
+            many=True,
+            context={'request': request}
+        )
+        return Response(serializer.data)
+
+    # handles GET one
+    def retrieve(self, request, pk=None):
+        """Handle GET requests for single product
+
+        Returns:
+            Response -- JSON serialized product instance
+        """
+        try:
+            product = Product.objects.get(pk=pk)
+            serializer = ProductSerializer(product, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
