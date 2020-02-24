cc8b398f734f49c9404355f63e369143f4f929f1
Jesie Oldenburg <kitglo46@gmail.com>
2020-02-24 11:55:36 -0600

    List and single resource methods working for orders. Added url paths and init file to acheive functionality

--- bangazon/urls.py
+++ bangazon/urls.py
@@ -14,8 +14,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
+from rest_framework import routers
+from rest_framework.authtoken.views import obtain_auth_token
+from bangazonAPI.models import *
+from bangazonAPI.views import *
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'orders', Orders, 'order')
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', include(router.urls)),
+    path('admin', admin.site.urls),
+    path('orders', Orders)
 ]
