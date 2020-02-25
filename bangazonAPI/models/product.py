eb00d140f7518319062b0630c3724ad23a8d19c0
Ryan Bishop <rdbishop06@gmail.com>
2020-02-25 12:19:01 -0600

    feat: product safe-delete added

--- bangazonAPI/models/product.py
+++ bangazonAPI/models/product.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.db.models import F
+from safedelete.models import SafeDeleteModel
+from safedelete.models import HARD_DELETE_NOCASCADE
 from .customer import Customer
 from .product_type import ProductType
 
 
-class Product(models.Model):
+class Product(SafeDeleteModel):
+    # Objects will be hard-deleted, or soft deleted if other objects would have been deleted too.
+    _safedelete_policy = HARD_DELETE_NOCASCADE
+    
     name = models.CharField(max_length=55)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     price = models.FloatField()
