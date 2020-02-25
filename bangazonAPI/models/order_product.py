16f736136819210b47bd14c7b24bee4a77b45895
Ryan Bishop <rdbishop06@gmail.com>
2020-02-25 13:21:45 -0600

    feat: user can delete products without cascade deleting them in order_products

--- bangazonAPI/models/order_product.py
+++ bangazonAPI/models/order_product.py
@@ -5,7 +5,7 @@ from .order import Order
 
 class OrderProduct(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
 
     class Meta:
         ordering = (F("order").desc(),)
--- bangazonAPI/models/product.py
+++ bangazonAPI/models/product.py
@@ -9,7 +9,7 @@ from .product_type import ProductType
 class Product(SafeDeleteModel):
     # Objects will be hard-deleted, or soft deleted if other objects would have been deleted too.
     _safedelete_policy = HARD_DELETE_NOCASCADE
-    
+
     name = models.CharField(max_length=55)
     customer = models.ForeignKey(Customer, on_delete=models.CASCADE)
     price = models.FloatField()
@@ -18,7 +18,7 @@ class Product(SafeDeleteModel):
     location = models.CharField(max_length=75, null=True)
     image_path = models.CharField(max_length=255)
     created_at = models.DateTimeField(auto_now=False, auto_now_add=True)
-    product_type = models.ForeignKey(ProductType, on_delete=models.SET_NULL, null=True)
+    product_type = models.ForeignKey(ProductType, on_delete=models.CASCADE, null=True)
 
     class Meta:
         ordering = (F("created_at").desc(), )
