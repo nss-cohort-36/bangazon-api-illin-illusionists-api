46d7e8fe4febbf24c36a0bbb6026376f57cd66d2
Melody Stern <melliemuse@gmail.com>
2020-03-04 15:23:15 -0600

    created custom function to check for an open order and to add the current product to it

--- bangazonAPI/models/order_product.py
+++ bangazonAPI/models/order_product.py
@@ -4,8 +4,8 @@ from .product import Product
 from .order import Order
 
 class OrderProduct(models.Model):
-    order = models.ForeignKey(Order, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name="cart")
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True, related_name="cart")
 
     class Meta:
         ordering = (F("order").desc(),)
