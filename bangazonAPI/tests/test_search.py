bb8983ca49ffa1d143941d9ec3e1499630ff0ac8
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-05 15:47:42 -0600

    New PR

--- bangazonAPI/tests/test_search.py
+++ bangazonAPI/tests/test_search.py
@@ -4,3 +4,12 @@ from django.urls import reverse
 from bangazonAPI.models import Product
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
+
+class TestSearch(TestCase):
+
+# Setting up a fake user to use for testing
+    def setUp(self):
+        self.username = 'testuser'
+        self.password = 'secureaf'
+        self.user = User.objects.create_user(username=self.username, password=self.password)
+        self.token = Token.objects.create(user=self.user)
