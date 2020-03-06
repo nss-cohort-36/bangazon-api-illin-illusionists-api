7401f6cc2a51c43305d39fc369851e47f267c781
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-06 15:23:51 -0600

    Typos

--- bangazonAPI/tests/test_search.py
+++ bangazonAPI/tests/test_search.py
@@ -20,6 +20,7 @@ class TestSearch(TestCase):
         self.customer = Customer.objects.create(user=self.user)
         self.product_type = ProductType.objects.create(name="Test Object")
     
+    # POST PRODUCT
     def test_post_product(self):
         # define a product to be sent to the API
         new_product = {
@@ -48,6 +49,7 @@ class TestSearch(TestCase):
         # And see if it's the one we just added by checking one of the properties. Here, name.
         self.assertEqual(Product.objects.get().name, 'Thneed')
     
+    # GET PRODUCT BY NAME
     def test_get_products_by_name(self):
         new_product = Product.objects.create(
               name = "Thneed",
@@ -69,7 +71,7 @@ class TestSearch(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # response.data is the python serialised data used to render the JSON, while response.content is the JSON itself.
-        # Are we responding with the data we asked for? There's just one pproduct in our dummy db, so it should contain a list with one instance in it
+        # Are we responding with the data we asked for? There's just one product in our dummy db, so it should contain a list with one instance in it
         self.assertEqual(len(response.data), 1)
 
         # test the contents of the data before it's serialized into JSON
@@ -79,6 +81,7 @@ class TestSearch(TestCase):
         # .encode converts from unicode to utf-8. Don't get hung up on this. It's just how we can compare apples to apples
         self.assertIn(new_product.name.encode(), response.content)
 
+# GET PRODUCT BY LOCATION
     def test_get_products_by_location(self):
         new_product = Product.objects.create(
               name = "Thneed",
@@ -100,7 +103,7 @@ class TestSearch(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # response.data is the python serialised data used to render the JSON, while response.content is the JSON itself.
-        # Are we responding with the data we asked for? There's just one pproduct in our dummy db, so it should contain a list with one instance in it
+        # Are we responding with the data we asked for? There's just one product in our dummy db, so it should contain a list with one instance in it
         self.assertEqual(len(response.data), 1)
 
         # test the contents of the data before it's serialized into JSON
@@ -110,6 +113,7 @@ class TestSearch(TestCase):
         # .encode converts from unicode to utf-8. Don't get hung up on this. It's just how we can compare apples to apples
         self.assertIn(new_product.name.encode(), response.content)
 
+# GET PRODUCT BY NAME AND LOCATION
     def test_get_products_by_name_and_location(self):
         new_product = Product.objects.create(
               name = "Thneed",
@@ -132,7 +136,7 @@ class TestSearch(TestCase):
         self.assertEqual(response.status_code, 200)
 
         # response.data is the python serialised data used to render the JSON, while response.content is the JSON itself.
-        # Are we responding with the data we asked for? There's just one pproduct in our dummy db, so it should contain a list with one instance in it
+        # Are we responding with the data we asked for? There's just one product in our dummy db, so it should contain a list with one instance in it
         self.assertEqual(len(response.data), 1)
 
         # test the contents of the data before it's serialized into JSON
