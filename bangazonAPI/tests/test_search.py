7e1cf68838483d05172f79ca3ee9501c815acc7f
Cassie Boyd <cassandra.bacon@gmail.com>
2020-03-06 11:54:22 -0600

    Added more tests to test_search

--- bangazonAPI/tests/test_search.py
+++ bangazonAPI/tests/test_search.py
@@ -10,7 +10,7 @@ from bangazonAPI.models import Customer
 
 class TestSearch(TestCase):
 
-# Setting up a fake user to use for testing
+# Setting up a fake user and foreign keys to use for testing
     def setUp(self):
         self.username = 'testuser'
         self.password = 'secureaf'
@@ -25,7 +25,7 @@ class TestSearch(TestCase):
               "name": "Thneed",
               "customer": self.customer.id,
               "price": 12.67,
-              "description": "A-fine-something-that-all-people need",
+              "description": "A fine something that all people need",
               "quantity": 1,
               "location": "Once-ler's Factory",
               "image_path": "UNLESS.jpg",
@@ -41,8 +41,42 @@ class TestSearch(TestCase):
         # Getting 200 back because we have a success url
         self.assertEqual(response.status_code, 200)
 
-        # Query the table to see if there's one ParkArea instance in there. Since we are testing a POST request, we don't need to test whether an HTTP GET works. So, we just use the ORM to see if the thing we saved is in the db.
+        # Query the table to see if there's one Product instance in there. Since we are testing a POST request, we don't need to test whether an HTTP GET works. So, we just use the ORM to see if the thing we saved is in the db.
         self.assertEqual(Product.objects.count(), 1)
 
         # And see if it's the one we just added by checking one of the properties. Here, name.
         self.assertEqual(Product.objects.get().name, 'Thneed')
+
+    def test_get_products(self):
+        new_product = Product.objects.create(
+              name = "Thneed",
+              customer = self.customer.id,
+              price = 12.67,
+              description = "A fine something that all people need",
+              quantity = 1,
+              location = "Once-ler's Factory",
+              image_path = "UNLESS.jpg",
+              product_type_id = self.product_type.id
+        )
+
+        # Now we can grab all the products (meaning the one we just created) from the db
+        response = self.client.get(reverse('product-list'))
+
+        # Check that the response is 200 OK.
+        # This is checking for the GET request result, not the POST. We already checked that POST works in the previous test!
+        self.assertEqual(response.status_code, 200)
+
+        # response.data is the python serialised data used to render the JSON, while response.content is the JSON itself.
+        # Are we responding with the data we asked for? There's just one pproduct in our dummy db, so it should contain a list with one instance in it
+        self.assertEqual(len(response.data), 1)
+
+        # test the contents of the data before it's serialized into JSON
+        self.assertEqual(response.data[0]["name"], "Thneed")
+
+        # Finally, test the actual rendered content as the client would receive it.
+        # .encode converts from unicode to utf-8. Don't get hung up on this. It's just how we can compare apples to apples
+        self.assertIn(new_product.name.encode(), response.content)
+
+
+if __name__ == '__main__':
+    unittest.main()
